52a888616d74e9b3af65cd02fe69c0a1abaff533
Arastoo Khajehee <a.khajehee@gmail.comls>
2026-05-11 12:19:24 +0900

    updated inline markdown for better legibility

--- lua/custom/plugins/render-markdown.lua
+++ lua/custom/plugins/render-markdown.lua
@@ -6,5 +6,60 @@ vim.pack.add({
     'https://github.com/MeanderingProgrammer/render-markdown.nvim',
 })
 require('render-markdown').setup({
-render_modes = { 'n', 'c', 't' },
+    render_modes = { 'n', 'c', 't' },
+    code = {
+        conceal_delimiters = false,
+        border = 'thin',
+    },
+    heading = {
+        backgrounds = {
+            'CursorLine',
+            'CursorLine',
+            'CursorLine',
+            'CursorLine',
+            'CursorLine',
+            'CursorLine',
+        },
+        foregrounds = {
+            'Normal',
+            'Normal',
+            'Normal',
+            'Normal',
+            'Normal',
+            'Normal',
+        },
+    },
 }) -- only mandatory if you want to set custom options
+
+local function normalize_markdown_heading_fg()
+    local normal = vim.api.nvim_get_hl(0, { name = 'Normal', link = false })
+    local fg = normal and normal.fg or nil
+    if not fg then
+        return
+    end
+
+    local groups = {
+        '@markup.heading',
+        '@markup.heading.1',
+        '@markup.heading.2',
+        '@markup.heading.3',
+        '@markup.heading.4',
+        '@markup.heading.5',
+        '@markup.heading.6',
+        '@markup.heading.1.markdown',
+        '@markup.heading.2.markdown',
+        '@markup.heading.3.markdown',
+        '@markup.heading.4.markdown',
+        '@markup.heading.5.markdown',
+        '@markup.heading.6.markdown',
+    }
+
+    for _, group in ipairs(groups) do
+        vim.api.nvim_set_hl(0, group, { fg = fg, bold = true })
+    end
+end
+
+normalize_markdown_heading_fg()
+vim.api.nvim_create_autocmd('ColorScheme', {
+    callback = normalize_markdown_heading_fg,
+})
