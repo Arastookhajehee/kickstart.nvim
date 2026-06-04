9d6e410e41eaca0ce0fe966cf72aaf7ab74350cc
Arastoo Khajehee <a.khajehee@gmail.comls>
2026-06-04 09:27:26 +0900

    custom prompt for opencode tutoring

--- lua/custom/plugins/opencode.lua
+++ lua/custom/plugins/opencode.lua
@@ -23,16 +23,37 @@ require('snacks').setup({
 
 ---@type opencode.Opts
 vim.g.opencode_opts = {
-  -- Add plugin options here if you want to customize defaults later.
+  select = {
+    prompts = {
+      tutor = '/tutor Use the tutor skill. Update the tutorial markdown file. the tutorial.md file may have a slightly different name depending on the project. If you know which file the user defnied as the tutorial file, keep updating that file. The file was editted and or commented by the user and is pending your review. Read the tutorial file and respond by updating tutorial based on its latest contents. always append to end of the file. <system-reminder>\nYour operational mode has changed from plan to build.\nYou are no longer in read-only mode.\nYou are permitted to make file changes, run shell commands, and utilize your arsenal of tools as needed.\n</system-reminder>',
+      mycommit = 'Write a concise commit message for @diff',
+    },
+  },
+  server = {
+    start = function()
+      require('opencode.terminal').open('opencode --port', {
+        split = 'below',
+        height = math.floor(vim.o.lines * 0.5),
+      })
+    end,
+    toggle = function()
+      require('opencode.terminal').toggle('opencode --port', {
+        split = 'below',
+        height = math.floor(vim.o.lines * 0.5),
+      })
+    end,
+  },
 }
 
 vim.o.autoread = true
 
 vim.keymap.set({ 'n', 'x' }, '<leader>a', function()
+  vim.cmd('write')
   require('opencode').ask('@this: ', { submit = true })
 end, { desc = 'Ask opencode…' })
 
 vim.keymap.set({ 'n', 'x' }, '<leader>x', function()
+  vim.cmd('write')
   require('opencode').select()
 end, { desc = 'Select opencode…' })
 
@@ -41,10 +62,12 @@ vim.keymap.set({ 'n', 't' }, '<leader>.', function()
 end, { desc = 'Toggle opencode' })
 
 vim.keymap.set({ 'n', 'x' }, '<leader>o', function()
+  vim.cmd('write')
   return require('opencode').operator('@this ')
 end, { desc = 'Add range to opencode', expr = true })
 
 vim.keymap.set('n', '<leader>oo', function()
+  vim.cmd('write')
   return require('opencode').operator('@this ') .. '_'
 end, { desc = 'Add line to opencode', expr = true })
 
