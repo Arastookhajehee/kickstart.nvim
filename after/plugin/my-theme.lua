fd9cd1ebc058e3c57f8c866084e5099fad30fc3d
Arastoo Khajehee <64447672+Arastookhajehee@users.noreply.github.com>
2026-08-10 08:34:07 +0900

    theme and uuid-generator

--- after/plugin/my-theme.lua
+++ after/plugin/my-theme.lua
@@ -1,5 +1,6 @@
 vim.pack.add({
   "https://github.com/EdenEast/nightfox.nvim",
+  "https://github.com/Mofiqul/dracula.nvim",
 })
 
 require("nightfox").setup({
@@ -14,4 +15,16 @@ require("nightfox").setup({
   },
 })
 
-vim.cmd.colorscheme("nordfox")
+require("dracula").setup({
+  options = {
+    -- transparent = true, -- remove this if you do not want transparency
+    terminal_colors = true,
+    styles = {
+      comments = "italic",
+      keywords = "bold",
+      types = "italic,bold",
+    },
+  },
+})
+
+vim.cmd.colorscheme("dracula")
---
+++ lua/custom/plugins/uuid-gen.lua
@@ -0,0 +1,7 @@
+vim.pack.add({
+    'https://github.com/TrevorS/uuid-nvim',
+})
+
+-- don't forget to call setup
+require('uuid-nvim').setup{}
+
