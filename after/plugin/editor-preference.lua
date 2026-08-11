2a4daaab6ca2999e453fa0215889ee6601bfcb54
Arastoo Khajehee <a.khajehee@gmail.com>
2026-08-11 13:03:49 +0900

    preference simplification

--- after/plugin/editor-preference.lua
+++ after/plugin/editor-preference.lua
@@ -1,1 +1,41 @@
+local os_type = vim.env.OS_TYPE
+
+vim.pack.add({
+  "https://github.com/EdenEast/nightfox.nvim",
+  "https://github.com/Mofiqul/dracula.nvim",
+})
+
+-- require("nightfox").setup({
+--   options = {
+--     -- transparent = true, -- remove this if you do not want transparency
+--     terminal_colors = true,
+--     styles = {
+--       comments = "italic",
+--       keywords = "bold",
+--       types = "italic,bold",
+--     },
+--   },
+-- })
+
+------   # THEME # ------- 
+require("dracula").setup({
+  options = {
+    -- transparent = true, -- remove this if you do not want transparency
+    terminal_colors = true,
+    styles = {
+      comments = "italic",
+      keywords = "bold",
+      types = "italic,bold",
+    },
+  },
+})
+
+vim.cmd.colorscheme("dracula")
+
+------   # TERMINAL # ------- 
+if os_type == "WIN" then
+  vim.opt.shell = "pwsh.exe"
+end
+
+------   # CODE APPEARANCE # ------- 
 vim.opt.wrap = false
